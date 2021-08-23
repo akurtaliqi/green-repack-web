7626e05302fc8523abb74d2833c51088a1340a2e
a.kurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-08-23 13:39:04 +0200

    first commit

---
+++ src/views/login/Index.vue
@@ -0,0 +1,29 @@
+<script>
+  // Extensions
+  import View from '@/views/View'
+
+  // Mixins
+  import LoadSections from '@/mixins/load-sections'
+
+  export default {
+    name: 'Login',
+
+    metaInfo: { title: 'Login' },
+
+    extends: View,
+
+    mixins: [
+      LoadSections([
+        'login-buyer',
+        'login-seller',
+      ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'recent-works',
+      },
+    },
+  }
+</script>
---
+++ src/views/sections/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <base-section id="gallery">
+    <v-tabs
+      v-model="tab"
+      centered
+      class="mb-12"
+    >
+      <v-tab
+        v-for="(tab, i) in tabs"
+        :key="i"
+        :href="`#${tab.toLowerCase()}`"
+        class="text-capitalize"
+      >
+        {{ tab }}
+      </v-tab>
+    </v-tabs>
+
+    <v-container>
+      <v-fade-transition
+        group
+        hide-on-leave
+        tag="div"
+        class="row"
+      >
+        <v-col
+          v-for="(project, i) in projects"
+          :key="project.title"
+          class="gallery-card"
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <project-card
+            v-bind="project"
+            :src="require(`@/assets/project-${i + 1}.jpg`)"
+            height="300"
+            width="100%"
+          />
+        </v-col>
+      </v-fade-transition>
+    </v-container>
+  </base-section>
+</template>
+
+<script>
+  export default {
+    name: 'SectionGallery',
+
+    components: {
+      ProjectCard: () => import('@/components/ProjectCard'),
+    },
+
+    data: () => ({
+      tab: 'All',
+      tabs: [
+        'Connection acheteur',
+        'Connection marchand',
+      ],
+    }),
+  }
+</script>
+
+<style lang="sass">
+  .gallery-card
+    transform-origin: center center 0
+    transition: .3s cubic-bezier(.25,.8,.50,1)
+</style>
