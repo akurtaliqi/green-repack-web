1968df6f5a164910de354d21c78c6bb39adb662c
akurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-10-04 19:29:31 +0200

    fix http commons

--- src/http-common/http-common.js
+++ src/http-common/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://test-green-repack-back.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
